9e4231ecd6ec3016db7ec081001ae97ba80081cf
Peng Hou <dustsnow@gmail.com>
2014-02-16 15:34:55 -0500

    Init; Able to detect push and swipe using broadcaster and listener structor; For swipe detector, to achieve more accurate detecting, there must be at least 1 second interval between every movement. Such as after swipe up, wait for 1 second then do another swipe

---
+++ src/old_openni_broadcaster.cpp
@@ -0,0 +1,21 @@
+#include <XnOpenNI.h>
+#include <XnCppWrapper.h>
+#include <XnVHandPointContext.h>
+#include <XnVSessionManager.h>
+#include <XnVBroadcaster.h>
+#include <XnVPushDetector.h>
+#include <XnVSteadyDetector.h>
+#include <XnVSwipeDetector.h>
+
+XnVSessionManager* g_pSessionManager = NULL;
+xn::Context g_Context;
+//XnVPushDetector g_PushDetector;
+//XnVSwipeDetector* m_pSwipeDetector = NULL;
+//XnVSteadyDetector* m_pSteadyDetector = NULL;
+
+int main(){
+
+	XnVBroadcaster g_Broadcaster;
+	return 0;
+}
+
